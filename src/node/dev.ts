2f08944bb60e65e3ab76f3d9c8530b3704d68f8f
weng.jiankai <weng.jiankai@iwhalecloud.com>
2022-11-17 16:24:22 +0800

    feat: 添加客户端渲染入口

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,10 @@
 import { createServer } from "vite";
+import react from "@vitejs/plugin-react";
+import pluginIndexHtml from "./plugin-island/indexHtml";
 
 export function createDevServer(root: string) {
   return createServer({
     root,
+    plugins: [pluginIndexHtml(), react()],
   });
 }
