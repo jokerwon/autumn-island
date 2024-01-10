f15090f652d0ebfa4a0439899300e0bdf440fe59
JokerWon <1595672103@qq.com>
2024-01-10 15:32:11 +0800

    feat: add e2e playwright

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,10 +1,16 @@
 import { createServer } from 'vite';
 import react from '@vitejs/plugin-react';
 import pluginIndexHtml from './plugin-island/indexHtml';
+import { PACKAGE_ROOT } from './constants';
 
 export function createDevServer(root: string) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml(), react()]
+    plugins: [pluginIndexHtml(), react()],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
+    }
   });
 }
